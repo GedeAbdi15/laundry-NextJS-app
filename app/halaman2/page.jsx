f2b65967189007dd2e5e4dace937b7cc6d8b3acf
GedeAbdi15 <abdi.wicaksana58@gmail.com>
2025-05-22 09:39:59 +0700

    first commit

---
+++ app/halaman2/page.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <h1 className="capitalize text-center">
+            HELLO WORLD, I'M USING NEXT JS "Halaman 2"
+        </h1>
+    );
+};
+
+export default Home;
